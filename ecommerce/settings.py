3b3f0ac271360a44ae088e293545cbf255f5b466
jesus <jesuspieru@gmail.com>
2022-10-24 22:49:13 -0400

    added models to leads app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leads.apps.LeadsConfig',
 ]
 
 MIDDLEWARE = [
@@ -109,3 +110,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'leads.User'
