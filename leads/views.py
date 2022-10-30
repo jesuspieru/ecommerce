ccde5e7f168a78e0e4694f3b92e190586dcd1d82
jesus <jesuspieru@gmail.com>
2022-10-29 20:39:02 -0400

    added details to leads on template

--- leads/views.py
+++ leads/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Lead
 
-def homepage(request):
+def lead_list(request):
     leads = Lead.objects.all()
     context = {
         "leads": leads
     }
-    return render(request, "leads/home.html", context)
+    return render(request, "leads/lead_list.html", context)
