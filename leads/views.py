17bd5694a8bc7d99dccd7b2294556bd9184361a0
jesus <jesuspieru@gmail.com>
2022-11-01 11:33:33 -0400

    added lead update and lead delete view

--- leads/views.py
+++ leads/views.py
@@ -22,18 +22,28 @@ def lead_create(request):
     if request.method == "POST":
         form = LeadForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            Lead.objects.create(
-                first_name=first_name,
-                last_name=last_name,
-                age=age,
-                agent=agent
-            )
+            form.save()
             return redirect("/leads")
     context = {
         "form": form
     }
     return render(request, "leads/lead_create.html", context)
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadForm(instance=lead)
+    if request.method == "POST":
+        form = LeadForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "form": form,
+        "lead": lead
+    }
+    return render(request, "leads/lead_update.html", context)
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
